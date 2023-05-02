e4f976fef08d3fc486bceafd61749b7da4c2f42d
Silver <gramakov27@gmail.com>
2023-05-02 18:22:48 +0300

    loading elements on click

--- src/js/modules/checkTextInputs.js
+++ src/js/modules/checkTextInputs.js
@@ -3,15 +3,10 @@ const checkTextInputs = (selector) => {
     const engChars = /[a-z]/ig;
 
     txtInputs.forEach(input => {
-        input.addEventListener('keypress', function(e) {
-            if (e.key.match(/[^а-яё 0-9]/ig)) {
-                e.preventDefault();
-            }
-        });
         input.addEventListener('input', function() {
             input.value = input.value.replace(/[^а-яё 0-9]/ig, '');
-        }) // очистка от 
-    });
+        }) // согласно ТЗ:
+    }); // Заполнение имени и комментария - только на РУССКОМ языке.
 };
 
 export default checkTextInputs;
