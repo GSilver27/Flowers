d4f4f1ef5439bc91997dc1570cd25e95c3d25721
Silver <gramakov27@gmail.com>
2023-05-09 14:52:34 +0300

    burger-menu

---
+++ src/js/modules/burger.js
@@ -0,0 +1,20 @@
+const burger = (menuSelector, burgerSelector) => {
+    const menuEl = document.querySelector(menuSelector),
+          burgerEl = document.querySelector(burgerSelector);
+
+    burgerEl.addEventListener('click', () => {
+        if (menuEl.style.display == 'none') {
+            menuEl.style.display = 'block';        
+        } else {
+            menuEl.style.display = 'none';
+        }
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.screen.availWidth >= 992) {
+            menuEl.style.display = 'none';
+        }
+    })
+};
+
+export default burger;
